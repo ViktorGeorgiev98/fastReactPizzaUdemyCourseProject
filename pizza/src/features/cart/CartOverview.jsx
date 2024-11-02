c51b78505e15ddacf222ef69c20cb6e4d2ddfa69
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-11-02 16:06:55 +0200

    cart overview state management

--- pizza/src/features/cart/CartOverview.jsx
+++ pizza/src/features/cart/CartOverview.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null;
+
   return (
-    <div className="bg-stone-800 text-stone-200 uppercase p-4 sm:px-4 md:text-base flex items-center justify-between">
-      <p className="text-stone=300 font-semibold space-x-4 sm:space-x-6">
+    <div className="flex items-center justify-between bg-stone-800 p-4 uppercase text-stone-200 sm:px-4 md:text-base">
+      <p className="text-stone=300 space-x-4 font-semibold sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
